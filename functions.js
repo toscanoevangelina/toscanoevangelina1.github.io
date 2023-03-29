ead7ce30a5e138f4eac10ed927c1065db01039a8
toscanoevangelina <125520343+toscanoevangelina@users.noreply.github.com>
2023-03-29 20:13:31 -0300

    Add files via upload

---
+++ functions.js
@@ -0,0 +1,6 @@
+
+document.getElementById('boton').onclick= function () {
+    console.log("Capturamos el evento click");
+    document.getElementById ("demo").innerHTML ="Poder trabajar en un ambiente lleno de desafios y comprometido a llegar a cumplirlos.Consolidarme en una empresa que me brinde estabilidad laboral.Lograr un ascenso después de haber cumplido un año en la compañía."
+    
+}
